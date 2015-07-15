f657759b88f420fce198be92ac2f9e134b0f8f4c
JVMR <jdkrmv@gmail.com>
2015-07-15 01:08:22 -0500

    historial

--- app/src/main/java/com/hoscanoa/developer/proyectodami/dao/historial/SQLiteHistorialDAO.java
+++ app/src/main/java/com/hoscanoa/developer/proyectodami/dao/historial/SQLiteHistorialDAO.java
@@ -1,8 +1,11 @@
 package com.hoscanoa.developer.proyectodami.dao.historial;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import com.hoscanoa.developer.proyectodami.beans.Historial;
+import com.hoscanoa.developer.proyectodami.conexion.DbHelper;
 
 import java.util.ArrayList;
 
@@ -19,12 +22,54 @@ public class SQLiteHistorialDAO implements HistorialDAO {
 
     @Override
     public ArrayList<Historial> listar() {
-        return null;
+        ArrayList<Historial> historials = new ArrayList<>();
+        try {
+            DbHelper helper = new DbHelper(context);
+            SQLiteDatabase database = helper.getReadableDatabase();
+            Cursor q = database.rawQuery("SELECT * FROM HISTORIAL",null);
+            Historial obj;
+            while (q.moveToNext())
+            {
+                obj = new Historial();
+                obj.setHistorialId(q.getInt(0));
+                obj.setProfesorId(q.getInt(1));
+                historials.add(obj);
+            }
+            q.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return historials;
     }
 
     @Override
     public Historial buscar(int id) {
+
         return null;
+    }
+
+
+    public Historial buscarProfesor(int idProfesor) {
+        Historial obj=null;
+        try {
+            DbHelper helper = new DbHelper(context);
+            SQLiteDatabase database = helper.getReadableDatabase();
+            Cursor q = database.rawQuery("SELECT * FROM HISTORIAL WHERE profesorId="+idProfesor,null);
+
+            while (q.moveToNext())
+            {
+                obj = new Historial();
+                obj.setHistorialId(q.getInt(0));
+                obj.setProfesorId(q.getInt(1));
+
+            }
+            q.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return obj;
     }
 
     @Override
